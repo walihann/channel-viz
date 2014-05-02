f898d909325f833f6e83d0b5012b2d2b594dbed3
walihann <hanno@walischewski.net>
2014-05-02 19:02:47 +0200

    added my keys

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'tDtSyn1vTFOHaChHQcAA6tvF27RjDhuNOZ9GGqRwmbzKGZAh', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [387123613], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Tracker', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
